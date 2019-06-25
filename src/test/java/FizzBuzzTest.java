a26001edf26196b25f09596866e73145b3441a63
hydra <dgrivas1990@gmail.com>
2019-06-25 17:25:00 -0400

    Change variable names to make them more readable

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,59 +4,59 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
-    private FizzBuzz fb;
-    private String text;
+    private FizzBuzz fizzBuzz;
+    private String outputMessage;
 
     @Before
     public void setUp() {
-        fb = new FizzBuzz();
+        fizzBuzz = new FizzBuzz();
     }
 
     @Test
     public void shouldBeFizzWhenIsMultipleOfThree() {
         //GIVEN
-        int nfizz = 3;
+        int testForFizz = 3;
         //WHEN
-        if (fb.isFizz(nfizz)) {
-            text = "Fizz";
+        if (fizzBuzz.isFizz(testForFizz)) {
+            outputMessage = "Fizz";
         }
         //THEN
-        assertEquals("Fizz", text);
+        assertEquals("Fizz", outputMessage);
     }
 
     @Test
     public void ShouldBeBuzzWhenIsMultipleOfFive() {
         //GIVEN
-        int nbuzz = 5;
+        int testForBuzz = 5;
         //WHEN
-        if (fb.isBuzz(nbuzz)) {
-            text = "Buzz";
+        if (fizzBuzz.isBuzz(testForBuzz)) {
+            outputMessage = "Buzz";
         }
         //THEN
-        assertEquals("Buzz", text);
+        assertEquals("Buzz", outputMessage);
     }
 
     @Test
     public void testShouldBeFizzBuzzWhenMultipleOfThreeAndFive() {
         //GIVEN
-        int nfb = 15;
+        int testForFizzBuzz = 15;
         //WHEN
-        if (fb.isFizz(nfb) && fb.isBuzz(nfb)) {
-            text = "FizzBuzz";
+        if (fizzBuzz.isFizz(testForFizzBuzz) && fizzBuzz.isBuzz(testForFizzBuzz)) {
+            outputMessage = "FizzBuzz";
         }
         //THEN
-        assertEquals("FizzBuzz", text);
+        assertEquals("FizzBuzz", outputMessage);
     }
 
     @Test
     public void testShouldPrintANumberIfItsNotAMultipleOfThreeNorFive() {
         //GIVEN
-        int nfb = 23;
+        int testNotFizzNorBuzz = 23;
         //WHEN
-        if (fb.isNotFizzBuzz(nfb)) {
-            text = nfb + "";
+        if (fizzBuzz.isNotFizzBuzz(testNotFizzNorBuzz)) {
+            outputMessage = testNotFizzNorBuzz + "";
         }
         //THEN
-        assertEquals(nfb + "", text);
+        assertEquals(testNotFizzNorBuzz + "", outputMessage);
     }
 }
