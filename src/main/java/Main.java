cef7e1c10e7deb6b1f722e69f609e3b5acb1804c
hydra <dgrivas1990@gmail.com>
2019-06-25 17:59:16 -0400

    Implement main method to run the program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int i = 1; i <= 100; i++) {
+            if (fizzBuzz.isFizz(i) && fizzBuzz.isBuzz(i)) {
+                System.out.println("FizzBuzz");
+            } else if (fizzBuzz.isBuzz(i)) {
+                System.out.println("Buzz");
+            } else if (fizzBuzz.isFizz(i)) {
+                System.out.println("Fizz");
+            } else if (fizzBuzz.isNotFizzBuzz(i)) {
+                System.out.println(i);
+            }
+        }
     }
-
 }
