51a4c61375ff0938c923a236f6ac131649553be7
hydra <dgrivas1990@gmail.com>
2019-06-25 16:45:48 -0400

    Create test class

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,25 @@
+import org.junit.*;
+
+public class FizzBuzzTest {
+
+    @Before
+    public void setUp() throws Exception {
+        throw new Exception();
+    }
+
+    @Test
+    public void shouldBeFizzWhenIsMultipleOfThree() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        throw new Exception();
+    }
+}
