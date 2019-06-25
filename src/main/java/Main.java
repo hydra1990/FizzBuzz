fb7697ea6de55f54279adf65b3f19f06be752b9b
hydra <dgrivas1990@gmail.com>
2019-06-25 17:21:13 -0400

    Delete comment LGTM by @Akiel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     }
     //Todo Please avoid committing the .idea folder as this configuration is only for your project on your computer (by @akiel)
     //Todo Please avoid committing not used/useful code. These tests down here do nothing (by @akiel)
-    //Todo LGTM (by Akiel)
     //Todo Please try using more explicit variable names (by Akiel)
     //Todo Please avoid adding binaries/libraries to the project if it's not strictly necesary (by Akiel)
 
